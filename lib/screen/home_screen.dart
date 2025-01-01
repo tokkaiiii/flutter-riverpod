e592c7e736471a20aa4287ad72c3e386e44b7a46
heosangbeom <luv_3_u@naver.com>
2025-01-01 12:53:32 +0900

    feat(riverpod): auto dispose modifier

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reverpod/layout/default_layout.dart';
+import 'package:reverpod/screen/auto_dispose_modifier_screen.dart';
 import 'package:reverpod/screen/family_modifier_screen.dart';
 import 'package:reverpod/screen/future_provider_screen.dart';
 import 'package:reverpod/screen/state_notifier_provider_screen.dart';
@@ -54,7 +55,8 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
-          ),ElevatedButton(
+          ),
+          ElevatedButton(
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
@@ -63,6 +65,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
           ),
         ],
       ),
