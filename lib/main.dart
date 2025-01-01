47c16a464f4902563b9c7177c5c2134871ecaca9
heosangbeom <luv_3_u@naver.com>
2025-01-01 10:50:50 +0900

    feat(riverpod-setting): riverpod 시작

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:reverpod/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
+
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:reverpod/layout/default_layout.dart';
+import 'package:reverpod/screen/state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => StateProviderScreen(),
+                  ),
+                );
+              },
+              child: Text('StateProviderScreen'))
+        ],
+      ),
+    );
+  }
+}
