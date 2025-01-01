af8c8021336412624b8c416127a2b01db3dbf780
heosangbeom <luv_3_u@naver.com>
2025-01-01 11:04:50 +0900

    feat(riverpod): riverpod 사용해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reverpod/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    )
+    ProviderScope(
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    ),
   );
 }
 
